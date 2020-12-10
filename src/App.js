7f0f2ce14d2651ba652b765edb87a6d0183d1fdd
moshecstern@gmail.com <moshecstern@gmail.com>
2020-12-10 10:03:43 -0500

    setting up app with material ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Homepage from './components/pages/Homepage.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import React, { useContext, useEffect } from 'react';
+// import AuthContext from '../../context/auth/authContext';
+const HomePage = () => {
+    // const authContext = useContext(AuthContext);
+
+    // useEffect(() => {
+    //     authContext.loadUser();
+    //     // eslint-disable-next-line
+    // }, []);
+
+    
+    return (
+        <>
+<div>
+    <h2>Welcome Home Page</h2>
+</div>
+</>
+)
+}
+export default HomePage;
