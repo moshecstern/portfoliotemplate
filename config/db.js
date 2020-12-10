beb89ac6f0d20aead01cb6174f3f100a9d47c4ab
moshecstern@gmail.com <moshecstern@gmail.com>
2020-12-10 14:31:01 -0500

    changed from yarn to npm, put in server and auth

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const config = require('config');
 // const db = config.get('mongoURI');
 // const db = 'mongodb+srv://<username>:<password>@counselinghr1.i0va3.mongodb.net/heroku_j88qppm5?retryWrites=true&w=majority';
-const db = process.env.MONGODB_URI || "mongodb://localhost:27017/healthcare";
+const db = process.env.MONGODB_URI || "mongodb://localhost:27017/portfoliomern";
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
